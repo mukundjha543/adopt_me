7e2a848b56a1b8a294104bdb4d695a2e744e654d
mukundjha543 <eemkjha@gmail.com>
2020-10-29 15:58:35 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Router } from "@reach/router";
+import SearchParams from './SearchParams';
+
+
+
+const App = () => {
+    return (
+        <div>
+          <h1>Adopt me!</h1>
+          <SearchParams />
+        </div>
+    )
+}
+
+render(
+    <App />,
+     document.getElementById("root")
+    );
