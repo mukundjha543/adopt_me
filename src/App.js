4e0cf21c598b740b934466c2576d035df5fa21f6
mukundjha543 <eemkjha@gmail.com>
2020-10-31 17:38:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { Router } from "@reach/router";
-import SearchParams from './SearchParams';
-
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { Router, Link } from "@reach/router";
+import Details from "./Details";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-    return (
-        <div>
-          <h1>Adopt me!</h1>
-          <SearchParams />
-        </div>
-    )
-}
-
-render(
-    <App />,
-     document.getElementById("root")
-    );
+  return (
+    <React.StrictMode>  
+    <div>
+      <header>
+        <Link to="/">Adopt Me!</Link>
+      </header>
+      <Router>
+        <SearchParams path="/" />
+        <Details path="/details/:id" />
+      </Router>
+    </div>
+    </React.StrictMode>
+  );
+};
+ReactDOM.render(<App />, document.getElementById("root"));
